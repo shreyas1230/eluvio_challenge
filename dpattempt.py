97c3577b196531f5ad2f4026f8c92e4890a0e4d9
Shreyas Agarwal <shreyas1230@hotmail.com>
2021-03-05 14:19:33 -0500

    updated dynamic programming solution

--- dpattempt.py
+++ dpattempt.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 import glob
 import time
+import IPython
+from IPython import embed
 
 from evaluate_sceneseg import *
 
@@ -13,7 +15,7 @@ def findSuperShots(weights, shots, preds, threshold=0):
     Inputs:
         weights - (N x 1) vector of weights for each shot
         shots - (N x D) matrix of shot representations
-        preds - length N-1 vector of predictions for each shot s whether shot boundary s:s+1 is a scene boundary
+        preds - (N-1) length vector of predictions for each shot s whether shot boundary s:s+1 is a scene boundary
 
     Output:
         out - (C x D) matrix of supershot representations where C is number of predicted supershots
@@ -29,81 +31,116 @@ def findSuperShots(weights, shots, preds, threshold=0):
     out = torch.zeros(count+1, idxs.shape[1], dtype=shots.dtype).scatter_add_(0, idxs, weights*shots)
     return out, [i for i in range(len(preds)) if preds[i] > threshold]
 
-def g(sshot_set):
+def g(corr):
     """
     Inputs:
-        sshot_set - (S x D) matrix of supershot representations
+        corr - (S x S) matrix of supershot correlations, S <= N
 
     Outputs:
         score - scalar optimal scene cut score achieved by sshot_set
     """
-    if len(sshot_set) == 0:
+    if len(corr) == 0:
         return 0
-    elif len(sshot_set) == 1:
+    if len(corr) == 1:
         return 0.5
-    cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-    arr = torch.clone(sshot_set)
+    N = corr.shape[0]
+    corr_offd = corr.masked_select(~torch.eye(N, dtype=bool)).view(N, N-1).T
+    Fs_total = torch.sum(corr_offd)/corr_offd.shape[0]
+    Ft_total = torch.sum(torch.sigmoid(torch.max(corr_offd, dim=0)[0]))
+    score = (Fs_total + Ft_total)/N
+    return score
+
+
+def get_g_mat(C):
+    """
+    Inputs:
+        C - (C x D) matrix of supershot representations
+
+    Outputs:
+        g_mat - (C x C) matrix of optimal scene cut score for every possible consecutive supershot groupings
+    """
+    arr = torch.clone(C)
     k = torch.linalg.norm(arr, dim=1)
     k = k.view((k.shape[0],1))
     arr = arr/k
-    new_arr = arr @ arr.T # correlation matrix
-    new_arr_offd = new_arr.masked_select(~torch.eye(sshot_set.shape[0], dtype=bool)).view(sshot_set.shape[0], sshot_set.shape[0]-1).T
-    Fs_total = torch.sum(new_arr_offd)/new_arr_offd.shape[0]
-    Ft_total = torch.sum(torch.sigmoid(torch.max(new_arr_offd, dim=0)[0]))
-    score = (Fs_total + Ft_total)/sshot_set.shape[0]
-    return score
+    corr = arr @ arr.T # correlation matrix
 
-def get_g_mat(C):
     g_mat = torch.zeros((C.shape[0], C.shape[0]))
     for col in range(C.shape[0]):
-        for row in range(1, C.shape[0]):
-            start = col+1
-            end = col+row+1
-            if end > C.shape[0]:
-                break
-
+        for row in range(C.shape[0]):
+            start = col
+            end = row+1
+            if end <= start:
+                continue
             idx = torch.arange(start, end)
-            g_mat[row, col] = g(torch.index_select(C, 0, idx))
+            val = g(corr[idx][:,idx])
+            g_mat[row, col] = val
     return g_mat
 
 def dp_sol(shots, initial_preds):
+    """
+    Inputs:
+        shots - (N x D) matrix of shot representations
+        initial_preds - (N-1) length vector of initial scene boundary predictions
+
+    Outputs:
+        new_preds - (N-1) length vector of scene boundary predictions
+    """
     W = Variable(torch.ones((shots.shape[0], 1)), requires_grad=True)
-    C, pred_bounds = findSuperShots(W, shots, initial_preds, threshold=0.9)
+    num_iterations = 6
+    predictions = initial_preds
+    for it in range(num_iterations):
+        print("Iteration: {}".format(it))
+        C, pred_bounds = findSuperShots(W, shots, predictions, threshold=0.7)
+        print(len(C))
+        G_mat = get_g_mat(C)
+        print(G_mat)
+        F = torch.zeros((C.shape[0], C.shape[0]-1, 2))
+        F[:, 0, 0] = G_mat[:,0]
 
-    G_mat = get_g_mat(C)
+        for j in range(1, F.shape[1]):
+            for i in range(j, F.shape[0]):
+                F[i, j, 0] = torch.max(F[:i, j-1, 0] + G_mat[i, 1:i+1])
+                F[i, j, 1] = torch.argmax(F[:i, j-1, 0] + G_mat[i, 1:i+1])
 
-    F = torch.zeros((C.shape[0], C.shape[0]-1, 2))
-    for row in range(C.shape[0]):
-        idx = torch.arange(row+1)
-        F[row,0,0] = g(torch.index_select(C, 0, idx))
+        ind = torch.argmax(F[-1,:,0])
+        lst_idxs = [F[-1, ind, 1].long().detach().item()]
+        for k in range(ind, -1, -1):
+            lst_idxs = [int(F[lst_idxs[0], k, 1].long().detach().item())] + lst_idxs
+        lst_idxs = torch.unique(torch.Tensor(lst_idxs), sorted=True)
+        # print(len(lst_idxs))
 
-    for j in range(1, F.shape[1]):
-        for i in range(F.shape[0]):
+        # embed()
+        # raise Exception
 
-            F[i, j, 0] = torch.max(F[:i+1, j-1, 0] + G_mat[torch.flip(torch.arange(i+1),[0]), torch.arange(i+1)])
-            F[i, j, 1] = torch.argmax(F[:i+1, j-1, 0] + G_mat[torch.flip(torch.arange(i+1),[0]), torch.arange(i+1)])
+        pred_idxs = []
+        prev = -1
 
-    lst_idxs = [F.shape[0]-1]
-    for k in range(F.shape[1]-1, -1, -1):
-        lst_idxs = [int(F[lst_idxs[0], k, 1].item())] + lst_idxs
+        # for i in lst_idxs:
+        #     if i != prev and i < len(pred_bounds):
+        #         pred_idxs.append(int(i))
+        new_pred_idxs = np.array(pred_bounds)[lst_idxs.long().tolist()]
 
-    lst_idxs = torch.unique(torch.Tensor(lst_idxs), sorted=True)
+        predictions = np.zeros(len(predictions))
+        # new_preds = np.array(predictions)
+        predictions[new_pred_idxs] = 1
 
-    pred_idxs = []
-    prev = -1
-
-    for i in lst_idxs:
-        if i != prev and i < len(pred_bounds):
-            pred_idxs.append(int(i))
-    new_pred_idxs = np.array(pred_bounds)[pred_idxs]
-
-    new_preds = np.zeros(len(initial_preds))
-    new_preds[new_pred_idxs] = 1
+        X = W.detach().clone()
+        value = torch.max(F[-1,:,0])
+        value.backward()
+        # print(torch.sum(W.grad.data)/W.grad.shape[0])
+        # raise Exception
+        W.data = W.data - 10 * W.grad.data
+        W.grad.data.zero_()
+        # print("Amount changed: {}".format(torch.sum((X-W)**2)))
+        # raise Exception
 
     # value = F[F.shape[0]-1, F.shape[1]-1, 0]
     # value.backward()
     # W.data = W.data - 0.005 * W.grad.data
     # W.grad.data.zero_()
+    initial_preds[predictions==1] = 1
+    new_preds = initial_preds
     return new_preds
 
 
@@ -123,10 +160,12 @@ for fn in filenames:
     predictions = x["scene_transition_boundary_prediction"].float()
     D = torch.cat([x["place"], x["cast"], x["action"], x["audio"]], dim=1)
     C = findSuperShots(Variable(torch.ones((D.shape[0], 1)), requires_grad=True), D, predictions, threshold=0.9)
-
+    # embed()
+    # raise Exception
 
     final_preds = dp_sol(D, predictions)
     pr_dict[x["imdb_id"]] = torch.Tensor(final_preds)
+    # break
 
 
 scores = dict()
